c1a8787c6fab10a8c0cd4e52f259b6e42fe1eca6
hongda <hongdago@yahoo.com>
2016-12-18 12:19:40 +0800

    简单的assert测试

--- 01-automated-testing/test/strftime_test.js
+++ 01-automated-testing/test/strftime_test.js
@@ -1,6 +1,14 @@
 var date = new Date(2009,11,5);
-console.log(date.strftime("%Y"));
-console.log(date.strftime("%m"));
-console.log(date.strftime("%d"));
-console.log(date.strftime("%y"));
-console.log(date.strftime("%F"));
+
+try{
+    assert("%Y shold return full year",date.strftime("%Y")==="2009");
+    assert("%m should return month",date.strftime("%m")==="12");
+    assert("%d should return date",date.strftime("%d")==="05");
+    assert("%y should return year as two digits",date.strftime("%y")==="09");
+    assert("%F should act as %Y-%m-%d",date.strftime("%F")==="2009-12-05");
+}catch(e){
+    console.log("Test failed:"+e.message);
+}
+
+output(assert.count +" tests OK","#0c0");
+output("Test failed:"+e.message,"#c00");
