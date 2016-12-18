5f979f012b4a32777bc4cdd45922ea58271fae1a
hongda <hongdago@yahoo.com>
2016-12-18 13:06:09 +0800

    testCase 添加setUp和tearDown

--- 01-automated-testing/lib/test_case.js
+++ 01-automated-testing/lib/test_case.js
@@ -2,14 +2,23 @@ function testCase(name, tests) {
     assert.count = 0;
     var sucessful = 0;
     var testCount = 0;
+    var hasSetup = typeof tests.setUp == "function";
+    var hasTeardown = typeof tests.tearDown == "function";
+    
     for (var test in tests) {
         if (!/^test/.test(test)) {
             continue;
         }
         testCount++;
         try {
+            if(hasSetup){
+                tests.setUp();
+            }
             tests[test]();
             output(test, "#0c0");
+            if(hasTeardown){
+                tests.tearDown();
+            }
             sucessful++;
         } catch (e) {
             output(test + " failed:" + e.message, "#c00");
--- 01-automated-testing/test/strftime_test.js
+++ 01-automated-testing/test/strftime_test.js
@@ -1,16 +1,18 @@
-var date = new Date(2009,11,5);
 testCase("strftime test",{
+    setUp:function(){
+        this.date = new Date(2009,11,5);
+    },
     "test format specifier %Y":function(){
-        assert("%Y shold return full year",date.strftime("%Y")==="2009");
+        assert("%Y shold return full year",this.date.strftime("%Y")==="2009");
     },
     "test format specifier %m":function(){
-        assert("%m should return month",date.strftime("%m")==="12");
+        assert("%m should return month",this.date.strftime("%m")==="12");
     },
     "test format specifier %y":function(){
-        assert("%y should return year as two digits",date.strftime("%y")==="09");
+        assert("%y should return year as two digits",this.date.strftime("%y")==="09");
     },
     "test format specifier %F":function(){
-        assert("%F should act as %Y-%m-%d",date.strftime("%F")==="2009-12-05");
+        assert("%F should act as %Y-%m-%d",this.date.strftime("%F")==="2009-12-05");
     }
 
 })
